4a15fc87d49346f9b34da3655dc408c24368a143
MrGoropeza <goropeza8@gmail.com>
2023-03-31 23:23:57 -0300

    feat: installed pocketbase sdk

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { AppComponent, APP_ROUTES } from './app/app.component';
+import PocketBase from 'pocketbase';
+import { APP_ROUTES, AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
     importProvidersFrom([BrowserAnimationsModule]),
+    {
+      provide: PocketBase,
+      useValue: new PocketBase('https://hpmi-refactor.pockethost.io'),
+    },
   ],
 }).catch((e) => console.error(e));
