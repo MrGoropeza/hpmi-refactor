a7bc35bed2a273d0f435330a5a1ebeda3bc6d9b5
MrGoropeza <goropeza8@gmail.com>
2023-04-02 02:47:57 -0300

    feat: delete selection method -> pharmacy articles table

--- src/app/pharmacy/feature/pharmacy-articles/pharmacy-articles.component.ts
+++ src/app/pharmacy/feature/pharmacy-articles/pharmacy-articles.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LazyLoadEvent, MenuItem } from 'primeng/api';
+import {
+  ConfirmationService,
+  LazyLoadEvent,
+  MenuItem,
+  MessageService,
+} from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
 import { InputTextModule } from 'primeng/inputtext';
@@ -9,7 +14,10 @@ import { TableModule } from 'primeng/table';
 import {
   BehaviorSubject,
   Observable,
+  catchError,
+  forkJoin,
   map,
+  of,
   skip,
   switchMap,
   take,
@@ -45,6 +53,15 @@ export class PharmacyArticlesComponent {
       skip(1),
       tap(() => this.loading$.next(true)),
       switchMap((event) => this.articlesService.listLazy(event)),
+      catchError(() => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: `Error cargando los registros. Recargá la página.`,
+        });
+
+        return of({});
+      }),
       tap(() => this.loading$.next(false))
     );
 
@@ -62,7 +79,7 @@ export class PharmacyArticlesComponent {
           icon: 'pi pi-trash',
           label: 'Borrar',
           disabled: selection.length === 0,
-          command: () => {},
+          command: () => this.deleteSelection(),
         },
       ];
     })
@@ -70,7 +87,9 @@ export class PharmacyArticlesComponent {
 
   constructor(
     private articlesService: PharmacyArticlesService,
-    private dialogService: DialogService
+    private dialogService: DialogService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
   ) {}
 
   openModal(row?: PharmacyArticleModel) {
@@ -86,5 +105,67 @@ export class PharmacyArticlesComponent {
     });
   }
 
-  delete(row: PharmacyArticleModel) {}
+  delete(row: PharmacyArticleModel) {
+    this.confirmationService.confirm({
+      header: `Eliminar "${row.name}"`,
+      message: '¿Estás seguro de eliminar el registro?',
+      accept: () => {
+        this.articlesService
+          .delete(row)
+          .pipe(take(1))
+          .subscribe({
+            complete: () => {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Éxito',
+                detail: `"${row.name}" borrado con éxito.`,
+              });
+              this.refresh$.next(this.refresh$.value);
+            },
+            error: () => {
+              this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: `Error al borrar "${row.name}". Intentá de nuevo.`,
+              });
+              this.loading$.next(false);
+            },
+          });
+      },
+    });
+  }
+
+  deleteSelection() {
+    this.confirmationService.confirm({
+      header: `Eliminar ${this.selection$.value.length} registros`,
+      message: '¿Estás seguro que querés eliminar los registros?',
+      accept: () => {
+        this.loading$.next(true);
+
+        const deleteOperations = this.selection$.value.map((selected) =>
+          this.articlesService.delete(selected)
+        );
+        this.selection$.next([]);
+
+        forkJoin(deleteOperations).subscribe({
+          complete: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: `Registros borrados con éxito.`,
+            });
+            this.refresh$.next(this.refresh$.value);
+          },
+          error: () => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Error al borrar los registros. Intentá de nuevo.`,
+            });
+            this.refresh$.next(this.refresh$.value);
+          },
+        });
+      },
+    });
+  }
 }
