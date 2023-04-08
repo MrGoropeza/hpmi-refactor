dc56e20b26a841925b884d94dca2778faeda653b
MrGoropeza <goropeza8@gmail.com>
2023-04-08 10:01:33 -0300

    feat: crud table action button component

---
+++ src/app/shared/ui/crud-table-action-button/crud-table-action-button.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
+
+@Component({
+  selector: 'crud-table-action-button',
+  standalone: true,
+  imports: [CommonModule, ButtonModule, TooltipModule],
+  template: `
+    <button
+      pButton
+      class="p-button-rounded p-button-raised {{ severityClass }}"
+      [pTooltip]="tooltip"
+      [tooltipPosition]="tooltipPosition"
+      [icon]="icon"
+      (click)="click.emit()"
+    ></button>
+  `,
+})
+export class CrudTableActionButtonComponent {
+  @Input() tooltip!: string;
+  @Input() tooltipPosition = 'top';
+  @Input() severity:
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'info'
+    | 'warning'
+    | 'help'
+    | 'danger' = 'primary';
+  @Input() icon = 'pi pi-play';
+  @Output() click = new EventEmitter();
+
+  get severityClass(): string {
+    return this.severity !== 'primary' ? `p-button-${this.severity}` : '';
+  }
+}
